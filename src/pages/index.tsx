08f96d4b285d530ff2e60b0f93c74ef4c13acb06
puko313 <pa.mendoza319@gmail.com>
2023-08-29 10:35:27 +0900

    next.js portfolio

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import { InferGetStaticPropsType } from 'next';
+import { RepositoryEdge } from 'generated/graphql';
+import Layout from 'components/ui/Layout';
+import SEO from 'components/SEO';
+import Intro from 'components/modules/Intro';
+import Projects from 'components/modules/Projects';
+import Skills from 'components/modules/Skills';
+import Contact from 'components/modules/Contact';
+
+const HomePage = ({ repos }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <Layout>
+    <SEO />
+    <Intro />
+    <Projects data={repos} />
+    <Skills />
+    <Contact />
+  </Layout>
+);
+
+export const getStaticProps = async () => {
+  const res = await axios({
+    url: 'https://api.github.com/graphql',
+    method: 'post',
+    data: {
+      query: `
+				query viewer {
+					viewer {
+						repositories(first: 8, orderBy: {field: STARGAZERS, direction: DESC}) {
+							edges {
+								node {
+									id
+									name
+									url
+									description
+									stargazers {
+										totalCount
+									}
+									forkCount
+									languages(first: 3) {
+										nodes {
+											id
+											name
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`,
+    },
+    headers: {
+      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  const repos: RepositoryEdge[] = res.data.data.viewer.repositories.edges;
+
+  return {
+    props: {
+      repos,
+    },
+    revalidate: 10,
+  };
+};
+
+export default HomePage;
